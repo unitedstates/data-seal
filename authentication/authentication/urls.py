3bd32595d8400493ff6e992f323e0a48f79395d3
Mike Tigas <mike@tig.as>
2014-05-06 17:56:49 -0400

    stub views; closes #4

--- authentication/authentication/urls.py
+++ authentication/authentication/urls.py
@@ -5,6 +5,11 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'authentication.authapp.views.index', name='index'),
+    url(r'^upload/$', 'authentication.authapp.views.upload', name='upload'),
+    url(r'^search/$', 'authentication.authapp.views.search', name='search'),
+    url(r'^file_detail/(?P<file_slug>\w+)/$', 'authentication.authapp.views.file_detail', name='file_detail'),
+    url(r'^file_download/(?P<file_slug>\w+)/$', 'authentication.authapp.views.file_download', name='file_download'),
+    url(r'^file_signature/(?P<file_slug>\w+)/signature/$', 'authentication.authapp.views.file_signature', name='file_signature'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
